7d5520233baa551bb8f5d75e27de452e136e820f
NathanAG <>
2021-05-22 20:55:28 +0000

    V1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+
+
+int main (){
+  int N,Max,Min;
+  cout << "Informe quantos números deseja analisar: ";
+  cin >> N;
+  int Vet[N];
+  cout << "Informe os "<<N<<" números:\n";
+  for(int i;i<N;i++){
+    cin >> Vet[i];
+  }
+  int *Ptr = Vet;
+
 }
