3eb5db992b106a0e4fc083128382ee17ec944474
NathanAG <>
2021-05-22 20:56:04 +0000

    V2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+void VerificNum(int N, int Vet[N], int *pMax, int *pMin){
+
+*pMax=Vet[0];
+*pMin=Vet[0];
+
+for(int i=1;i<N;i++){
+
+}
+
+int main (){
+  int N,Max,Min;
+  cout << "Informe quantos números deseja analisar: ";
+  cin >> N;
+  int Vet[N];
+  cout << "Informe os "<<N<<" números:\n";
+  for(int i;i<N;i++){
+    cin >> Vet[i];
+  }
+  int *Ptr = Vet;
+  VerificNum(N, Ptr, &Max, &Min);
+
 }
